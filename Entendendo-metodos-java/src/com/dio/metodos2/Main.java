7a07d2c2624866986da34dc2f23593e1ac72bdba
Joaovr5 <jjoaovitorr.rrocha@gmail.com>
2022-10-05 01:08:40 -0300

    commit final

---
+++ Entendendo-metodos-java/src/com/dio/metodos2/Main.java
@@ -0,0 +1,16 @@
+package com.dio.metodos2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	        // Quadrilátero
+	        System.out.println("Exercício quadrilátero");
+	        Sobrecarga.area(3);
+	        Sobrecarga.area(5d,5d);
+	        Sobrecarga.area(7,8,9);
+	        Sobrecarga.area(5f,5f);
+
+	}
+
+}
